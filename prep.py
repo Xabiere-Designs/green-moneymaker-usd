cd29d798c8d53d53a042c44db7a09de44254f023
Xabiere-Designs <xabieredesigns@gmail.com>
2023-07-03 04:20:43 +0000

    prep

--- prep.py
+++ prep.py
@@ -2,10 +2,11 @@ import random
 import uuid
 import os
 
-def generate_file(name = None, size = random.randint(3, 10)):
-    if(name == None):  # Check if the name parameter is not provided
+def generate_file(name: str = None, size: int = random.randint(1, 10)) -> None:
+    if name is None:  # Check if the name parameter is not provided
         name = str(uuid.uuid4())[:8] + '.dummy'  # Generate a random name using UUID if not provided
     with open(name, 'wb') as fout:  # Open the file in binary write mode
         fout.write(os.urandom(size))  # Write random bytes to the file
     fout.close()  # Close the file
+
 
