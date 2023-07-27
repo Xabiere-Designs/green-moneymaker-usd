f69a3a9424f962da5fe732693525caeb32824a2b
Xabiere-Designs <xabieredesigns@gmail.com>
2023-07-26 23:02:39 -0400

    ec2_jenkins_sample

--- Jenkins_Terraform/ec2.tf
+++ Jenkins_Terraform/ec2.tf
@@ -20,7 +20,7 @@ data "aws_availability_zones" "available_zones" {}
 
 # create default subnet if one does not exit
 resource "aws_default_subnet" "default_az1" {
-  availability_zone = 
+  availability_zone = data.aws_availability_zones.available_zones.name[0]
 
   tags   = {
     Name = "default subnet"
@@ -32,33 +32,33 @@ resource "aws_default_subnet" "default_az1" {
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 80 and 22"
-  vpc_id      = 
+  vpc_id      = aws_default_vpc.default_vpc.id 
 
   ingress {
-    description      = "http access"
-    from_port        = 
-    to_port          = 
-    protocol         = 
-    cidr_blocks      = 
+    description      = "http proxy access"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = "0.0.0.0/0"
   }
 
   ingress {
     description      = "ssh access"
-    from_port        = 
-    to_port          = 
-    protocol         = 
-    cidr_blocks      = 
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = "0.0.0.0/0"
   }
 
   egress {
-    from_port        = 
-    to_port          = 
-    protocol         = 
-    cidr_blocks      = 
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = "0.0.0.0/0"
   }
 
   tags   = {
-    Name = 
+    Name = "jenkins server security group"
   }
 }
 
@@ -82,10 +82,10 @@ data "aws_ami" "amazon_linux_2" {
 
 # launch the ec2 instance and install website
 resource "aws_instance" "ec2_instance" {
-  ami                    = 
-  instance_type          = 
-  subnet_id              = 
-  vpc_security_group_ids = 
+  ami                    = data "aws_ami.amazon_linux_2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   key_name               = 
   user_data              = 
 
