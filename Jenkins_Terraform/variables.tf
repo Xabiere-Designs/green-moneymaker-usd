e72304702caee435af3e4a81f716c7b68ed053ef
Xabiere-Designs <xabieredesigns@gmail.com>
2023-09-13 04:00:54 +0000

    variables_change

--- Jenkins_Terraform/variables.tf
+++ Jenkins_Terraform/variables.tf
@@ -8,3 +8,9 @@ variable "instance_type" {
   type    = string
   default = "t2.micro"
 }
+variable "aws_access_key" {
+    default = "AKIATOKGAW3IYQKQE242"
+}
+variable "aws_secret_key" {
+    default = "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"
+}
