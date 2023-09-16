4bd3c0009007dabca664db45cbe7e81a62a05fb5
Xabiere-Designs <xabieredesigns@gmail.com>
2023-09-16 00:58:56 +0000

    game changer

--- Jenkins_Terraform/main.tf
+++ Jenkins_Terraform/main.tf
@@ -4,3 +4,15 @@ resource "aws_instance" "app_server" {
   ami           = var.aws_ami
   instance_type = var.instance_type
 
+<<<<<<< Updated upstream
+=======
+  tags = {
+    Service   = "EC2"
+    ManagedBy = "Jenkins"
+  }
+}
+
+
+
+
+>>>>>>> Stashed changes
