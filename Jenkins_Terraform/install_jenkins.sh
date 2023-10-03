7cf964445c1a4e8079ad555cf7322619c09dd04e
Xabiere-Designs <xabieredesigns@gmail.com>
2023-10-03 04:30:07 +0000

    install_update

--- Jenkins_Terraform/install_jenkins.sh
+++ Jenkins_Terraform/install_jenkins.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-sudo yum update -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
+sudo apt-get update -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
 
