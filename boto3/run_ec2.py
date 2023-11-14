be7d8fcf1856f3aff9747b2bfd74871a80cc66b9
Xabiere-Designs <xabieredesigns@gmail.com>
2023-11-14 21:29:43 +0000

    updated run ec2 file

--- boto3/run_ec2.py
+++ boto3/run_ec2.py
@@ -1,10 +1,40 @@
 import boto3
 
+# Create an EC2 resource
 ec2 = boto3.resource('ec2')
 
-instances = ec2.create_instances(
-    ImageId='ami-06ca3ca175f37dd66',
+# Create development instances
+dev = ec2.create_instances(
+    ImageId='ami-05c13eab67c5d8861',
     MaxCount=3,
     MinCount=3,
     InstanceType='t2.micro',
-   )
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'Name', 'Value': 'Development'},
+                {'Key': 'Environment', 'Value': 'Development'}
+            ]
+        },
+    ],
+)
+
+# Create production instances
+prod = ec2.create_instances(
+    ImageId='ami-05c13eab67c5d8861',
+    MaxCount=1,
+    MinCount=1,
+    InstanceType='t2.micro',
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'Name', 'Value': 'Production'},
+                {'Key': 'Environment', 'Value': 'Production'}
+            ]
+        },
+    ],
+)
+
+
